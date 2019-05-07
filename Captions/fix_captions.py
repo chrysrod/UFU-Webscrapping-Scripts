825ebd32644b241d65ea9d6f3b26b29b4008e4ed
Chrystian R. Campos <chrystianrcampos@gmail.com>
2019-05-07 16:55:03 -0300

    Improves

--- Captions/fix_captions.py
+++ Captions/fix_captions.py
@@ -4,10 +4,25 @@ f = open (sys.argv[1],'r')
 file = f.readlines()
 f.close()
 
-file_write = open(sys.argv[1],'a')
+file_new = open(sys.argv[1],'w')
+
+text_tag = "conforme a figura abaixo:"
+text_tag_2 = "conforme a imagem abaixo:"
+caption_tag = "<figcaption>"
+referencia = 0
+figura = 1
 
 for line in file:
-    if "conforme a figura abaixo:" in line:
-        print(line[line.find('conforme'):])
+        
+        if (text_tag in line) or (text_tag_2 in line):
+                file_new.write(line[:line.find('conforme')] + 'conforme a Figura ' + str(figura) + ":" + '\n')
+                referencia = 1
+        elif (caption_tag in line) and referencia == 1:
+                file_new.write(line[:line.find('<figcaption>')] + '<figcaption>Figura ' + str(figura) + ' - ' + 
+                line[line.find('<figcaption>')+12:line.find('</figcaption>')] + '</figcaption>' + '\n')
+                referencia = 0
+                figura += 1
+        else:
+                file_new.write(line)             
 
-file_write.close()
+file_new.close()
